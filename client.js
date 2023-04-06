22ed40138a05cddf85ae6eda534605b4b47f2308
varun8100s <varunraveesh@gmail.com>
2023-04-06 14:02:09 +0530

    first push

--- client.js
+++ client.js
@@ -1,25 +1,30 @@
+const os = require("os")
 const io = require("socket.io-client");
 const socket = io("http://localhost:3000");
 
 socket.on("connect", () => {
-  console.log("Connected to server!");
+    console.log("Connected to server!");
+    socket.emit(
+      "deviceInfo",
+      `System Name: ${os.hostname()}  , Username: ${os.userInfo().username}`
+    );
 });
 
-// Listen for messages from the server
-// socket.on("message", (message) => {
-//   console.log(`Received message from server: ${message}`);
-// });
+// disconnect from server
+socket.on('disconnect', () => {
+  console.log('Disconnected from server.');
+});
 
-// Send a message to the server
-socket.emit("message", "Hello, server!");
-
-
+//pendrive detection
 var monitor = require("node-usb-detection");
 
 monitor.add(function (device) {
-  console.log("USB DEVICE INSERTED:\n", device);
+    console.log("USB DEVICE INSERTED:\n", device);
+    socket.emit("usb",{"status":"inserted","device":device});
 });
 
 monitor.remove(function (device) {
-  console.log("USB DEVICE REMOVED:\n", device);
+    console.log("USB DEVICE REMOVED:\n", device);
+    socket.emit("usb", { status: "removed", device: device });
 });
+ 
--- server.js
+++ server.js
@@ -1,22 +1,42 @@
 const port = process.env.PORT || 3000
 const io = require("socket.io")(port);
-console.log(`Port selected is: ${port}`);
+console.log(`Control Panel\nPort selected is: ${port}`);
+
+const allSystems = []
+
+// console.log(`1) View all systems online \n2) Exit`)
 
 io.on("connection", (socket) => {
-  console.log("A client has connected!");
-
-  // Send a message to the client
-  socket.emit("message", "Hello, client!");
-
-  // Listen for messages from the client
-  socket.on("message", (message) => {
-    console.log(`Received message from client: ${message}`);
+  
+  // Listen for system info from the client
+  socket.on("deviceInfo", (message) => {
+      console.log(`${message} CONNECTED`);
+      allSystems.push({ id: socket.id, deviceInfo: message })
+      console.log("Systems online: "+allSystems.length)
   });
-
+    
+    
   // Handle disconnection
-  socket.on("disconnect", () => {
-    console.log("A client has disconnected!");
+  socket.on("disconnect", (msg) => {
+      
+      for (i in allSystems) {
+          if (socket.id == allSystems[i].id) {
+              console.log(allSystems[i].deviceInfo+" DISCONNECTED")  
+              allSystems.splice(i, 1)
+          }
+      }
+      console.log("Systems online: " + allSystems.length);
   });
+    
+    //listen to usb changes reported
+    socket.on("usb", (logmsg) => {
+        for (i in allSystems) {
+          if (socket.id == allSystems[i].id) {
+            console.log("======USB CHANGES DETECTED======\n",allSystems[i].deviceInfo );
+          }
+        }
+        console.log(logmsg)
+    })
 });
 
 
